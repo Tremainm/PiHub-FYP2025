43cc1ae3bfbc6e6a5ab76f4d4e0da3f3f0c33222
Tremainm <147380462+Tremainm@users.noreply.github.com>
2025-10-28 16:01:23 +0000

    Added one-to-many relationship - one user can have multiple devices

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import func, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import func, String, ForeignKey
 from datetime import datetime
 
 class Base(DeclarativeBase):
@@ -10,12 +10,16 @@ class UserDB(Base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String, nullable=False)
     email: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
+    devices: Mapped[list["DeviceDB"]] = relationship(back_populates="owner", cascade="all, delete-orphan")
 
+# One-to-many relationship (One User can have Many devices)
 class DeviceDB(Base):
     __tablename__ = "devices"
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     status: Mapped[str] = mapped_column(String, nullable=False, default="OFF")
+    owner_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    owner: Mapped["UserDB"] = relationship(back_populates="devices")
 
 # class SensorReading(Base):
 #     __tablename__ = "sensor_readings"
