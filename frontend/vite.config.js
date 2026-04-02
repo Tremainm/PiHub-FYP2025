8c93a2b69dcca932ec2fcef1f4a249565269f449
Tremain <g00367973@atu.ie>
2026-04-02 08:41:16 +0100

    Setting up vitest frontend testing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',   // simulate browser DOM
+    globals: true,          // no need to import describe/it/expect in every file
+    setupFiles: './src/test/setup.js',
+  }
 })
